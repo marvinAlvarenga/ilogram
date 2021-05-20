2c16512b6d44d482584f1824312f0245685e245f
Marvin Alvarenga <marvinalexz@gmail.com>
2021-05-19 22:57:21 -0600

    Create a simple view to return a list of posts

--- posts/urls.py
+++ posts/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 
+from .views import list_posts
+
 
 urlpatterns = [
+    path('', list_posts, name='list'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.utils.timezone import now
 
-# Create your views here.
+
+posts = [
+    {
+        'name': 'Marvin Ramirez',
+        'user': 'mRamirez',
+        'timestamp': str(now()),
+    }
+] * 3
+
+
+def list_posts(request):
+    content = (
+        """
+        <p><strong>{name}</strong></p>
+        <p><small>{user} - <i>{timestamp}</i></small></p>
+        """.format(**post)
+        for post in posts
+    )
+
+    return HttpResponse('<br>'.join(content))
